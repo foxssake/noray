84ee38b1dfc864207fbf72bb0f85d33c1fcad8f6
Tamás Gálffy <ezittgtx@gmail.com>
2023-05-29 23:41:17 +0200

    Cleanup config

--- src/config.mjs
+++ src/config.mjs
@@ -15,25 +15,10 @@ export class NorayConfig {
     port: integer(env.NORAY_SOCKET_PORT) ?? 8808
   }
 
-  session = {
-    timeout: duration(env.NORAY_SESSION_TIMEOUT ?? '1hr'),
-    cleanupInterval: duration(env.NORAY_SESSION_CLEANUP_INTERVAL ?? '10m')
-  }
-
-  lobby = {
-    minNameLength: number(env.NORAY_LOBBY_MIN_NAME_LENGTH) ?? 3,
-    maxNameLength: number(env.NORAY_LOBBY_MAX_NAME_LENGTH) ?? 128
-  }
-
-  connectionDiagnostics = {
-    timeout: duration(env.NORAY_CONNECTION_DIAGNOSTICS_TIMEOUT ?? '8s')
-  }
-
   udpRelay = {
     maxSlots: number(env.NORAY_UDP_RELAY_MAX_SLOTS) ?? 16384,
     timeout: duration(env.NORAY_UDP_RELAY_TIMEOUT ?? '30s'),
     cleanupInterval: duration(env.NORAY_UDP_RELAY_CLEANUP_INTERVAL ?? '30s'),
-    registrarPort: number(env.NORAY_UDP_REGISTRAR_PORT) ?? 8809,
 
     maxIndividualTraffic: byteSize(env.NORAY_UDP_RELAY_MAX_INDIVIDUAL_TRAFFIC ?? '128kb'),
     maxGlobalTraffic: byteSize(env.NORAY_UDP_RELAY_MAX_GLOBAL_TRAFFIC ?? '1gb'),
@@ -41,8 +26,6 @@ export class NorayConfig {
     maxLifetimeDuration: duration(env.NORAY_UDP_RELAY_MAX_LIFETIME_DURATION ?? '4hr'),
     maxLifetimeTraffic: byteSize(env.NORAY_UDP_RELAY_MAX_LIFETIME_TRAFFIC ?? '4gb')
   }
-
-  games = env.NORAY_GAMES ?? ''
 
   loglevel = getLogLevel()
 }
