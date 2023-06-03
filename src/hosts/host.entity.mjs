f25c0e9a11ba3f53eb3c879336070e5202ddd493
Tamás Gálffy <ezittgtx@gmail.com>
2023-06-03 18:05:15 +0200

    Add entity

---
+++ src/hosts/host.entity.mjs
@@ -0,0 +1,38 @@
+import { nanoid } from 'nanoid'
+import * as net from 'node:net'
+
+/**
+* Host entity.
+*
+* Hosts register in advance for other players to connect to them.
+*/
+export class HostEntity {
+  /**
+  * Open id.
+  * @type {string}
+  */
+  oid
+
+  /**
+  * Private id.
+  * @type {string}
+  */
+  pid
+
+  /**
+  * Socket.
+  * @type {net.Socket}
+  */
+  socket
+
+  /**
+  * Construct entity.
+  * @param {HostEntity} options Options
+  */
+  constructor (options) {
+    options && Object.assign(this, options)
+
+    this.oid ??= nanoid()
+    this.pid ??= nanoid(128)
+  }
+}
