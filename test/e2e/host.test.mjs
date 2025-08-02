49aa426e512a31c15b485ffdc5fe47f9e545c8ad
Tamás Gálffy <ezittgtx@gmail.com>
2025-08-02 18:29:48 +0200

    Revert "skip all"
    This reverts commit 8ec37502dd69618f12c8e9ad87643092b59d4b4f.

--- test/e2e/host.test.mjs
+++ test/e2e/host.test.mjs
@@ -2,7 +2,7 @@ import { describe, it, before, after } from 'node:test'
 import assert from 'node:assert'
 import { End2EndContext } from './context.mjs'
 
-describe('Hosts', { skip: (isCI() ? "Skipping in CI until #50 is fixed" : undefined)}, () => {
+describe('Hosts', () => {
   const context = new End2EndContext()
 
   before(async () => {
